8753c93826adaee98e7f908b6aa00dbf1d5f2aa5
Russell-Ding <zirui_ding@mfe.berkeley.edu>
2017-05-19 10:07:39 -0700

    Update 0314_return_analysis.py

--- 0314_return_analysis.py
+++ 0314_return_analysis.py
@@ -6,11 +6,12 @@ import setting
 from portfolio_analysis import  review_pf_return
 import io_tool
 
+# Read the pre-defined signal and calculate return based on that
+
 def read_in_signal(name):
     """Read in signal from data prepared"""
     m_data = pd.read_csv(setting.datapath_prepared+name+'.csv')
     m_data['date'] = pd.to_datetime(m_data['date'])
-#     m_data_pivot =pd.pivot_table(m_data,columns=['PERMNO'],index=['date'],values=[name])
     m_data_pivot =pd.pivot_table(m_data,columns='PERMNO',index='date',values=name)
     return m_data_pivot
 
@@ -18,7 +19,8 @@ def read_in_signal(name):
 
 return_pivot=read_in_signal('M_RET')
 pf_start = datetime.datetime(1965, 2, 1)
-pf_end = datetime.datetime(2016, 12, 31)     
+pf_end = datetime.datetime(2016, 12, 31)   
+#Amend the file name if necessary
 pfweight = io_tool.load_pickle_obj(setting.datapath_pf_weight, 
 	'pfweight_FF_M_VOL_lag_False1965-02-2016-12')
 return_series=review_pf_return(pf_start, pf_end, pfweight, return_pivot)
